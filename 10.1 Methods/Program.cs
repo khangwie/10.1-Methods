64b55a508982efcd7a997637e09e19a56ea78c87
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 19:16:27 +0200

    Add project files.

---
+++ 10.1 Methods/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._1_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi();
+
+            SayHiAge("John", 55);
+
+            SayHiUser("Evans");
+
+            Console.ReadLine();
+
+        }
+        static void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+        static void SayHiUser(string name)
+        {
+            Console.WriteLine("Hello " + name);
+        }
+        static void SayHiAge(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + "you are " + age);
+
+            Console.ReadLine();
+        }
+    }
+}
